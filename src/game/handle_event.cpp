67e3ad8bdc95351d44efc7d70ba80133e6f9339f
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-08-04 08:51:57 +0300

    Adding default keybind: Press Q to quit

--- src/game/handle_event.cpp
+++ src/game/handle_event.cpp
@@ -1,11 +1,13 @@
 #include "game/game.hpp"
+#include "core/core.hpp"
 #include <SDL2/SDL_events.h>
 
 
 template<>
-void game::handle_event(SDL_KeyboardEvent&)
+void game::handle_event(SDL_KeyboardEvent& ev)
 {
-
+    if(ev.keysym.sym == SDLK_q)
+        core::stop();
 }
 
 
