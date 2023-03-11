8cd50c61fbc528018936b02cc6a0f6f0ac7e9783
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-03-11 04:19:25 +0200

    config: Adding animation speed

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -5,6 +5,12 @@ bool config::quick_draw = true;
 
 #include "config/game.hpp"
 
-bool config::detect_diagonal_lines = false;
+bool config::allow_diagonal_moves = true;
+bool config::detect_diagonal_lines = !false;
 bool config::requeue_waiting_moves = false;
+bool config::permit_free_moves = false;
+
+int config::move_duration = 120; // ms
+int config::break_duration = 120; // ms
+
 
--- src/config/game.hpp
+++ src/config/game.hpp
@@ -3,10 +3,28 @@
 
 namespace config
 {
+    /* Mechanics */
+
+    // Wether to allow diagonal tile swaps
+    extern bool allow_diagonal_moves; // TODO use
+
+    // Wether to detect lines on diagonals
     extern bool detect_diagonal_lines;
 
     // Wether to requeue or drop moves on busy tiles
     extern bool requeue_waiting_moves;
+
+    // Free moves are moves that don't form a line
+    extern bool permit_free_moves;
+
+
+    /* Animations*/
+
+    // Move animation duration in ms
+    extern int move_duration;
+
+    // Line break animation duration in ms
+    extern int break_duration;
 }
 
 #endif // INCLUDE_CONFIG_GAME_HPP
