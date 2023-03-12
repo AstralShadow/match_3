bee4d2cbf2e09575eae0afec5c1bcd0a5b4224f7
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-03-12 03:31:06 +0200

    fix: is_tile_falling should not count last tiles
    Last tile in FallingTiles.tiles is stored just
     for animations to know tile starting position.
    It points to invalid position above the board
     or an empty tile.

--- src/game/is_tile_falling.cpp
+++ src/game/is_tile_falling.cpp
@@ -5,8 +5,13 @@
 bool game::is_tile_falling(Point pos)
 {
     for(auto const& group : falling_tiles) {
-        for(auto const& other : group.tiles)
-            if(other == pos)
+        if(group.tiles.size() < 2)
+            continue;
+
+        // Ignore top-most tile as it's meant
+        //  for animation rendering purposes
+        for(u32 i = 0; i < group.tiles.size() - 1; ++i)
+            if(group.tiles[i] == pos)
                 return true;
     }
 
