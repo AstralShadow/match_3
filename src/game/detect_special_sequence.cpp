d0394fe1a08a74dca3d80747bbbad1cac85927f2
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-04-20 21:57:28 +0300

    Revert "game: Activating Beam skill on player move (experimental)"
    This reverts commit 07745ae520f2a81f7c5705bc23f09b066fadb3b2.

--- src/game/detect_special_sequence.cpp
+++ src/game/detect_special_sequence.cpp
@@ -1,5 +1,4 @@
 #include "game/line_sequence.hpp"
-#include "game/skills/beam.hpp"
 #include <iostream>
 
 using std::cout;
@@ -8,19 +7,9 @@ using std::endl;
 
 void game::detect_special_sequence(LineSequence* data)
 {
-    if(data->lines.size() < 1)
-        return;
-
-    auto const& last_line = data->lines.back();
-
-    Point direction = last_line[0] - last_line[1];
-    active_skills.push_back(std::make_unique
-        <Beam>(last_line[0], direction)
-    );
-    data->lines.pop_back();
-
     // TODO: implement
-    
+
+
     if(log_sequences_to_stdout) {
         int number = data - &line_sequences[0];
         cout << "Sequence " << number
